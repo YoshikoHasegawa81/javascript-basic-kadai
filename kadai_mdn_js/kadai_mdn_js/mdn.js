3397801beb4cec41b74ed4b34ac6688613911b85
HY445 <yochi.candy@gmail.com>
2025-09-04 11:12:56 +0900

    js修正しました。

--- kadai_mdn_js/kadai_mdn_js/mdn.js
+++ kadai_mdn_js/kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth()+1;
-const day = date.getDate();
+const executionDate = new Date();
+const year = executionDate.getFullYear();
+const month = executionDate.getMonth()+1;
+const day = executionDate.getDate();
 
 console.log(year + '年' + month + '月' + day + '日');
